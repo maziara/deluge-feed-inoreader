6dbaff2376c585a0c5cdb8537ab2f8066109ce66
doubleossa <doubleossa@gmail.com>
2015-11-02 22:26:59 +0000

    Added exception handeling to remove the added items from inoreader, even if exception occurs in the middle.

--- main.py
+++ main.py
@@ -9,17 +9,25 @@ def go_process():
         print "Retreived " + str(len(items['items'])) + " starred items"
         client = delugeapi.connect_to_deluge()
         for item in items['items']:
-            print "Saving -> '" + item['title'] + "'"
-            tor_id = client.add_torr_url(inoreaderapi.get_enclosure_url(item))
-            if tor_id: #URL added, otherwise it's probably already in deluge!
-                label = item['origin']['title']
+            try:
+                print "Saving -> '" + item['title'] + "'"
+                tor_id = client.add_torr_url(inoreaderapi.get_enclosure_url(item))
+                if tor_id: #URL added, otherwise it's probably already in deluge!
+                    label = item['origin']['title']
 
-                if not client.label_exist(label):
-                    client.add_label(label, {'move_completed_path': inoreaderapi.generate_save_path(item)})
+                    if not client.label_exist(label):
+                        client.add_label(label, {'move_completed_path': inoreaderapi.generate_save_path(item)})
 
-                client.add_tor_label(tor_id, label )
-
-        inoreaderapi.toggle_labels([i['id'] for i in items['items']])
+                    client.add_tor_label(tor_id, label )
+                item['saved'] = True
+            except:
+                print 'Failed on processing: ' + item['title']
+                raise
+                #print 'Debug item content: '
+                #print item
+            finally:
+                inoreaderapi.toggle_labels([i['id'] for i in items['items'] if i.has_key('saved')])
+        
 
 if __name__ == "__main__":
     go_process()
