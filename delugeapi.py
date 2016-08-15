68df7417d37f5b53360f34a8e9dcbc7e02d48c1a
doubleossa <doubleossa@gmail.com>
2016-08-15 19:40:51 +0000

    Methods added to remove finished torrs.

--- delugeapi.py
+++ delugeapi.py
@@ -50,6 +50,45 @@ def connect_to_deluge():
         return self.call('label.set_torrent', tor_id, normalize_label(label))
     client.add_tor_label = MethodType(add_tor_label, client, DelugeRPCClient)
 
+    def session_state(self):
+        return self.call('core.get_session_state')
+    client.session_state = MethodType(session_state, client, DelugeRPCClient)
+    
+    def torrent_status(self, tid, fields = {}):
+        return self.call('core.get_torrent_status', tid, fields)
+    client.torrent_status = MethodType(torrent_status, client, DelugeRPCClient)
+
+    def torrents_status(self, filters = {}, fields = []):
+        return self.call('core.get_torrents_status', filters, fields)
+    client.torrents_status = MethodType(torrents_status, client, DelugeRPCClient)
+    
+    def get_finished(self):
+        torrs = torrents_status(self)
+        for k,v in torrs.items():
+            #print(k,v['name'])
+            if v['is_finished'] == False:
+                #print("Removing unfinished: " + v['name'] + " " + str(v['is_finished']))
+                torrs.pop(k)
+            elif v['tracker_host'] in config.REMOVE_SEEDS_EXCEPTION_TRACKERS:
+                #print("Removing exception_tracker: " + v['name'])
+                torrs.pop(k)
+            elif not is_all_files_done(v):
+                #print("Removing not_all_done: " + v['name'])
+                torrs.pop(k)
+        return torrs
+    client.get_finished = MethodType(get_finished, client, DelugeRPCClient)
+    
+    def remove_finished(self):
+        for k in get_finished(self):
+            self.call('core.remove_torrent', k, False)
+    client.remove_finished = MethodType(remove_finished, client, DelugeRPCClient)
+    
+    def is_all_files_done(tor):
+        for i in tor['file_progress']:
+            if i != 1.0: 
+                return False
+        return True
+
     return client
 
 def normalize_label(label):
